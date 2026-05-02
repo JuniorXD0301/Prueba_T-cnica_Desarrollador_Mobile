cac11b061450f5502fb22505b4f9c2d3538fae7f
JuniorXD0301 <juniorcorredor2@gmail.com>
2026-05-02 10:35:32 -0500

    ajustar scroll

--- todo-app/src/app/shared/shared.module.ts
+++ todo-app/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { CategoryTaskCountPipe } from '../pipes/category-task-count.pipe';
 import { TaskCategoryFilterPipe } from '../pipes/task-category-filter.pipe';
@@ -7,7 +8,7 @@ import { VirtualScrollComponent } from './virtual-scroll/virtual-scroll.componen
 
 @NgModule({
   declarations: [CategoryTaskCountPipe, TaskCategoryFilterPipe, VirtualScrollComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ScrollingModule],
   exports: [CategoryTaskCountPipe, TaskCategoryFilterPipe, VirtualScrollComponent],
 })
 export class SharedModule {}
--- todo-app/src/app/shared/virtual-scroll/virtual-scroll.component.ts
+++ todo-app/src/app/shared/virtual-scroll/virtual-scroll.component.ts
@@ -9,8 +9,14 @@ import { Component, ContentChild, Input, TemplateRef } from '@angular/core';
 export class VirtualScrollComponent<T> {
   @Input() items: T[] = [];
   @Input() itemHeight = '72px';
+  @Input() maxHeight = 'calc(100vh - 180px)';
 
   @ContentChild(TemplateRef) itemTemplate?: TemplateRef<{ $implicit: T; item: T; index: number }>;
+
+  get itemSize(): number {
+    const parsed = Number.parseInt(this.itemHeight, 10);
+    return Number.isNaN(parsed) ? 72 : parsed;
+  }
 
   trackByIndex(index: number): number {
     return index;
